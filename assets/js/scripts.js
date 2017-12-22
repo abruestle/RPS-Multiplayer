4665b49ad33b9543f41c0e1d3c2d27144c52f3b2
abruestle <anna@bruestle.net>
2017-12-21 21:22:12 -0500

    some save/load firebase, still need finding specific user

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,36 +10,123 @@
 
 	//Chat
 
+
+var database = firebase.database();
+var connectionsRef= database.ref("/connections");
+var connectedRef= database.ref(".info/connected");
+
 var game = {
 	//General variables (to start with) - mostly for the individual
 	player: {
 		//ID
 		name: "",
+		UID: 0,
 		//All time game info
 		wins: 0,
 		losses: 0,
 		ties: 0,
 		//current game information:
-		lastChoice: -1,
 		lastChat: "",
 		lastChatTime: 0,
 		//Initializes as not logged in (0), as initially you are not logged in
 		loggedIn: 0,
 	},
-	//Game variables (offline)
-	lastCondition: "",
-	currentcondition: "",
+	enemy: {
+		name: "",
+		wins: 0,
+		losses: 0
+	},
+	//Game variables
+	player1: -1,
+	player2: -1,
+	lastChoice1: -1,
+	lastChoice2: -1,
+	//1 for P1 won, 2 for P2 won, 3 for tie, 0 at reset/no game going on
+	currentCondition: 0,
 	options: ["r", "p", "s"],
-	data: firebase.database(),
+	onlinePeople: 0,
+	players: 0,
 	//Initialize with Firebase
-	initialize: function(){
+	loadFirebaseGame: function(snapshot){
+		//Load data from firebase
+		game.player1 = snapshot.val().player1;
+		game.player2 = snapshot.val().player2;
+		game.lastChoice1 = snapshot.val().lastChoice1;
+		game.lastChoice2 = snapshot.val().lastChoice2;
+		game.currentCondition = snapshot.val().currentCondition;
+		game.onlinePeople = snapshot.val().onlinePeople;
+		game.players = snapshot.val().players;
+		
+	},
+	loadFirebaseEnemy: function() {
+		//Loads name and wins/losses directly into html
+		game.enemy.name = snapshot.val().name;
+		game.enemy.wins = snapshot.val().wins;
+		game.enemy.losses = snapshot.val().losses;
+	},
+	saveFirebaseGame: function() {
+		database.ref().set({
+			player1: game.player1,
+			player2: game.player2,
+	        lastChoice: game.lastChoice,
+	        currentCondition: game.currentCondition,
+	        onlinePeople: game.onlinePeople,
+	        players: game.players
+	      });
+	},
+	initializeFull: function(){
 		//clear chat
 		$("#chat dl").empty();
-		//check local storage for saved username/password
-		//if none, disable chat, game buttons, and rps and ask for signing up or logging on
-		//get from firebase database
+		$("#loginComments").text("Enter your name so you can join a game!");
+		$('#chatEnter').attr('disabled', ' disabled');
+		$('#joinGame').attr('disabled', ' disabled');
+		game.reset();
+		loadFirebaseGame();
+	},
+	reset: function() {
+		//disable chatting, go to sign up/login/join page
+		$('#actions button').attr('disabled', ' disabled');
+		$("#player").html('<img alt="Player Choice" src="assets/images/player.png"><div class="caption"><h3>Player</h3><div class="progress"><div class="progress-bar progress-success" role="progressbar" id="PlayerWinsLosses" style="width:100%"></div></div></div>');
+		$("#enemy").html('<img alt="Oppenent Choice" src="assets/images/player.png"><div class="caption"><h3>Player</h3><div class="progress"><div class="progress-bar progress-success" role="progressbar" id="PlayerWinsLosses" style="width:100%"></div></div></div>');
+
+		//Goes to login tab (just in case it was the opponent who said something)
+			$("#login-tab a").click();
+			$(".tab-content").attr("class","tab-content");
 	},
 	//Sign In
+	login: function() {
+		//check if user is listed in database
+		//if not, add them
+		game.player.name = $("#user").text().trim();
+		$("#loginComments").text("Hello new user, " + game.player.name + ".");
+		//Adds user as a new person
+		game.onlinePeople++;
+		dataRef.ref().push({
+
+	        player.name: game.player.name,
+			name: "",
+			UID: 0,
+			//All time game info
+			wins: 0,
+			losses: 0,
+			ties: 0,
+			//current game information:
+			lastChoice: -1,
+			lastChat: "",
+			lastChatTime: 0,
+			//Initializes as not logged in (0), as initially you are not logged in
+			loggedIn: 0,
+      	});
+		// database.ref().set({
+		// 	users: game.player1,
+		// 	player2: game.player2,
+	 //        lastChoice: game.lastChoice,
+	 //        currentCondition: game.currentCondition,
+	 //        onlinePeople: game.onlinePeople,
+	 //        players: game.players
+	 //      });
+		
+	},
 	//Game
 	play: function(action, person) {
 		//play the game! Either player or opponent, and actually acts if both are ready
@@ -70,7 +157,7 @@ var game = {
 				$("#chat dl").append('<dt>'+person.trim()+'</dt>');
 			}
 			//Chat dialog is added from whoever said the chat
-			$("#chat dl").append('<dd>'+input.trim()+'</dd>');
+			$("#chat dl").append('<dd>  '+moment(currentTime).format("hh:mm")+'  '+input.trim()+'</dd>');
 
 			///Goes to chat tab (just in case it was the opponent who said something)
 			$("#chat-tab a").click();
@@ -82,22 +169,40 @@ var game = {
 		//Firebase ^_^
 		//local storage saves name and password for easier logging in
 		//saves that you are logged in
-	},
-	//On disconnect: you don't auto-lose (as rps doesn't need rules like that), but your data needs to be recorded and you need to be counted as offline
-	//Login error
-	notLoggedIn: function() {
-		//gives not logged in message; switches to right page, disables functions
+		localStorage.setItem("name", game.player.name);
+		localStorage.setItem("password", game.player.password);
+		localStorage.setItem("UID", game.player.UID);
+		game.player.loggedin = 1;
 	}
+	//On disconnect: you don't auto-lose (as rps doesn't need rules like that), but your data needs to be recorded and you need to be counted as offline
 
 }
 
+database.ref().on("value", function(snapshot) {
+  // We are now inside our .on function...
+
+  // Console.log the "snapshot" value (a point-in-time representation of the database)
+  console.log(snapshot.val());
+  // This "snapshot" allows the page to get the most current values in firebase.
+
+
+// If any errors are experienced, log them to console.
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
 
 
 
-// $("body").on("click", ".char", function() {
-// 	game.chooseChar($(this).attr("data-name"));
 
-// });
+game.initializeFull();
+
+//Login
+
+$("#nameEnter").on("click", function() {
+	game.login();
+});
+
+
 
 $("#actions .btn").on("click", function() {
 	game.chat(this.val());
@@ -128,53 +233,70 @@ $("#actions .btn").on("click", function() {
 	});
 //Opponent does rps events
 
+// When the client's connection state changes...
+	connectedRef.on("value", function(snap) {
 
+	  // If they are connected..
+	  if (snap.val()) {
 
+	    // Add user to the connections list.
+	    var con = connectionsRef.push(true);
+	    // Remove user from the connection list when they disconnect.
+	    con.onDisconnect().remove();
+	  }
+	});
 
+	// When first loaded or when the connections list changes...
+	connectionsRef.on("value", function(snap) {
+
+	  // Display the viewer count in the html.
+	  // The number of online users is the number of children in the connections list.
+	  $("#connected-viewers").text(snap.numChildren());
+	});
 
 
 
 
 //previous RPS code
 
-document.onkeyup = function (event) {
-    var userChoice = options.indexOf(event.key);
-    var compChoice = Math.floor((Math.random() * 3));
-    if (userChoice == -1) {
-        document.getElementById("alert").innerHTML = "Please Choose r (rock), p (paper) or s (scissor)";
-    } else {
-        document.getElementById("alert").innerHTML = "";
-        
-        if (compChoice == userChoice) {
-            tie++;
-            currentcondition = "tie";
-        } else if (userChoice == compChoice + 1 || userChoice == compChoice - 2){
-            // Checks if win from being right after compChoice in array or rock/scissors win)
-            wins++;
-            currentcondition = "win";
-        } else {
-            losses++;
-            currentcondition = "loose";
-        }
+	// document.onkeyup = function (event) {
+	//     var userChoice = options.indexOf(event.key);
+	//     var compChoice = Math.floor((Math.random() * 3));
+	//     if (userChoice == -1) {
+	//         document.getElementById("alert").innerHTML = "Please Choose r (rock), p (paper) or s (scissor)";
+	//     } else {
+	//         document.getElementById("alert").innerHTML = "";
+	        
+	//         if (compChoice == userChoice) {
+	//             tie++;
+	//             currentcondition = "tie";
+	//         } else if (userChoice == compChoice + 1 || userChoice == compChoice - 2){
+	//             // Checks if win from being right after compChoice in array or rock/scissors win)
+	//             wins++;
+	//             currentcondition = "win";
+	//         } else {
+	//             losses++;
+	//             currentcondition = "loose";
+	//         }
 
-    }
+	//     }
 
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("ties").innerHTML = tie;
-    document.getElementById("key").innerHTML = options[userChoice];
-    document.getElementById("compchoice").innerHTML = options[compChoice];
-    document.getElementById("current").innerHTML = "You " + currentcondition + ".";
+	//     document.getElementById("wins").innerHTML = wins;
+	//     document.getElementById("losses").innerHTML = losses;
+	//     document.getElementById("ties").innerHTML = tie;
+	//     document.getElementById("key").innerHTML = options[userChoice];
+	//     document.getElementById("compchoice").innerHTML = options[compChoice];
+	//     document.getElementById("current").innerHTML = "You " + currentcondition + ".";
 
 
-   // win condition
-    if (currentcondition == "tie") {
-        wincondition++;
-    }
-    if (losses >= wincondition) {
-        alert("you lose");
-    }
-    if (wins >= wincondition) {
-        alert("you win");
-    }
-}
+	//    // win condition
+	//     if (currentcondition == "tie") {
+	//         wincondition++;
+	//     }
+	//     if (losses >= wincondition) {
+	//         alert("you lose");
+	//     }
+	//     if (wins >= wincondition) {
+	//         alert("you win");
+	//     }
+	// }
